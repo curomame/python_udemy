d706e1758125ad7fe70857a26b9af0be2c70f171
curomame <curomame@naver.com>
2023-01-05 00:11:00 +0900

    day2

---
+++ day2/ex2-1.py
@@ -0,0 +1,6 @@
+two_digit_number = input("Type a two digit number: ")
+
+first = int(str(two_digit_number)[0])
+second = int(str(two_digit_number)[1])
+
+print(first+second)
---
+++ day2/ex2-3.py
@@ -0,0 +1,3 @@
+age = int(input("What is your current age?"))
+
+print(f"You have {90*365 - age * 365} days, {90*52 - age * 52} weeks, and {90*12 - age * 12} months left.")
